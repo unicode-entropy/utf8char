e257564fe867ee98fd56f54fa0a44e6905880e29
ultrabear <bearodark@gmail.com>
2024-10-26 22:29:34 -0700

    TODO fill in these stubs

--- src/charapi.rs
+++ src/charapi.rs
@@ -22,30 +22,29 @@ impl fmt::Display for Utf8Char {
 }
 
 // TODO(ultrabear): implement all of these
-// skip if the implementation would fallback to to_char().method()
+// skip if the implementation would be faster as to_char().method()
 #[allow(unused)]
 impl Utf8Char {
-    const fn eq_ignore_ascii_case(self, other: Self) -> bool {
-        todo!()
-    }
+    const fn eq_ignore_ascii_case(self, other: Self) -> bool { todo!() }
 
-    fn make_ascii_lowercase(&mut self) {
-        todo!()
-    }
-
-    const fn to_ascii_lowercase(self) -> Self {
-        todo!()
-    }
-
-    const fn is_ascii(self) -> bool {
-        todo!()
-    }
-
-    const fn is_ascii_alphabetic(self) -> bool {
-        todo!()
-    }
+    const fn is_ascii(self) -> bool { todo!() }
+    const fn is_ascii_alphabetic(self) -> bool { todo!() }
+    const fn is_ascii_alphanumeric(self) -> bool { todo!() }
+    const fn is_ascii_control(&self) -> bool { todo!() }
+    const fn is_ascii_digit(&self) -> bool { todo!() }
+    const fn is_ascii_graphic(&self) -> bool { todo!() }
+    const fn is_ascii_hexdigit(&self) -> bool { todo!() }
+    const fn is_ascii_lowercase(&self) -> bool { todo!() }
+    const fn is_ascii_punctuation(&self) -> bool { todo!() }
+    const fn is_ascii_uppercase(&self) -> bool { todo!() }
+    const fn is_ascii_whitespace(&self) -> bool { todo!() }
 
 
+    fn make_ascii_lowercase(&mut self) { todo!() }
+    fn make_ascii_uppercase(&mut self) { todo!() }
+    const fn to_ascii_lowercase(self) -> Self { todo!() }
+    const fn to_ascii_uppercase(self) -> Self { todo!() }
 
-
+    const fn is_digit(self, radix: u8) -> bool { todo!() }
+    const fn to_digit(self, radix: u8) -> Option<u8> { todo!() }
 }
