28af5acd805731096f5177b354d245f88a3a04f1
ultrabear <bearodark@gmail.com>
2024-11-10 02:41:31 -0800

    change bench to just from_first_char_unchecked

--- benches/default.rs
+++ benches/default.rs
@@ -7,21 +7,26 @@ pub fn bench(c: &mut Criterion) {
     let mut buf = [0; 4];
     let st = &*bb(rch.encode_utf8(&mut buf));
     let utf8char = Utf8Char::from_char(rch);
+    let nonempty = bb("\u{ff00}fsgsdg");
 
-    c.bench_function("str.next_char", |c| {
-        c.iter(|| unsafe { bb(bb(st).chars().next().unwrap_unchecked()) })
-    });
+//    c.bench_function("str.next_char", |c| {
+//        c.iter(|| unsafe { bb(bb(st).chars().next().unwrap_unchecked()) })
+//    });
 
-    c.bench_function("utf8char::as_char", |c| {
-        c.iter(|| bb(bb(utf8char).to_char()))
-    });
+//    c.bench_function("utf8char::as_char", |c| {
+//        c.iter(|| bb(bb(utf8char).to_char()))
+//    });
 
-    c.bench_function("utf8char::from_char", |c| {
-        c.iter(|| bb(Utf8Char::from_char(bb(rch))))
-    });
+//    c.bench_function("utf8char::from_char", |c| {
+//        c.iter(|| bb(Utf8Char::from_char(bb(rch))))
+//    });
 
-    c.bench_function("encode_unicode::Utf8Char::new", |c| {
-        c.iter(|| bb(encode_unicode::Utf8Char::new(bb(rch))))
+//    c.bench_function("encode_unicode::Utf8Char::new", |c| {
+//        c.iter(|| bb(encode_unicode::Utf8Char::new(bb(rch))))
+//    });
+
+    c.bench_function("utf8char::from_first_char_unchecked", |c| {
+        c.iter(|| bb(unsafe { Utf8Char::from_first_char_unchecked(nonempty) }))
     });
 }
 
