f597502891c08b15320dc57e0ae5c942fd10ce78
ultrabear <bearodark@gmail.com>
2025-03-31 20:36:23 -0700

    add Utf8CharFirstByte and Utf8CharEncodedLength types

--- src/iter.rs
+++ src/iter.rs
@@ -2,20 +2,71 @@
 
 use core::{
     hint::assert_unchecked,
+    marker::PhantomData,
     ptr::{self, NonNull},
 };
 
 use crate::Utf8Char;
 
+/// An iterator over a string that yields `Utf8Char`'s
 // modeled after slice::Iter
-pub struct Utf8CharIter {
+pub struct Utf8CharIter<'slice> {
     /// start pointer that is incremented on iter::next
     ptr: NonNull<u8>,
     /// end pointer that is beyond the provenance range
     end: *const u8,
+
+    /// Phantom lifetime of our backing string
+    lifetime: PhantomData<&'slice str>,
 }
 
-impl Utf8CharIter {
+#[derive(Copy, Clone, Debug)]
+#[repr(u8)]
+enum Utf8CharEncodedLength {
+    One = 1,
+    Two,
+    Three,
+    Four,
+}
+
+/// The first and potentially only byte of a utf8 encoded codepoint
+// Safety invariant: `u8 is 0..0xff` & `u8 is only a firstbyte of a utf8 codepoint`
+#[repr(transparent)]
+struct Utf8CharFirstByte(u8);
+
+impl Utf8CharFirstByte {
+    /// Constructs a new `Utf8CharFirstByte` from a `Utf8Char`'s first byte
+    fn from_utf8char(c: Utf8Char) -> Self {
+        Self(c.0.first_byte())
+    }
+
+    /// Constructs a new Utf8CharFirstByte from an arbitrary u8
+    ///
+    /// # Safety
+    /// value passed must be a valid utf8 encoded characters first byte
+    unsafe fn new(b: u8) -> Self {
+        Self(b)
+    }
+
+    fn codepoint_len(self) -> Utf8CharEncodedLength {
+        let len: u8 = Utf8Char::codepoint_len(self.0);
+
+        // SAFETY: Utf8CharEncodedLength is repr(u8), Utf8Char::codepoint_len will return 1..=4 for
+        // any valid utf8 codepoint first byte, which this type stores as a safety invariant
+        unsafe { core::mem::transmute(len) }
+    }
+}
+
+impl<'slice> Utf8CharIter<'slice> {
+    /// Fills a buffer with 1..=4 bytes from the backing slice, advancing the iterator
+    ///
+    /// # Safety
+    /// There must be at least `n` bytes available in the backing iterator
+    unsafe fn fill_buf(&mut self, buf: &mut [u8; 4], n: Utf8CharEncodedLength) {}
+
+    /// Constructs a new `Utf8CharIter` from a string slice, borrowing the slice
+    fn new(s: &'slice str) -> Self {}
+
     /// # Safety
     /// There must be at least one more codepoint in the backing utf8 slice
     unsafe fn next_unchecked(&mut self) -> Utf8Char {
