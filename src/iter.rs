af93e182b6c06f13445a41317b220692024f25ca
ultrabear <bearodark@gmail.com>
2025-03-31 20:57:52 -0700

    add more Utf8CharIter methods

--- src/iter.rs
+++ src/iter.rs
@@ -1,8 +1,8 @@
 //! Implements an Iterator on strings that provide Utf8Char's
 
 use core::{
-    hint::assert_unchecked,
     marker::PhantomData,
+    mem,
     ptr::{self, NonNull},
 };
 
@@ -14,7 +14,7 @@ pub struct Utf8CharIter<'slice> {
     /// start pointer that is incremented on iter::next
     ptr: NonNull<u8>,
     /// end pointer that is beyond the provenance range
-    end: *const u8,
+    end: NonNull<u8>,
 
     /// Phantom lifetime of our backing string
     lifetime: PhantomData<&'slice str>,
@@ -53,7 +53,7 @@ impl Utf8CharFirstByte {
 
         // SAFETY: Utf8CharEncodedLength is repr(u8), Utf8Char::codepoint_len will return 1..=4 for
         // any valid utf8 codepoint first byte, which this type stores as a safety invariant
-        unsafe { core::mem::transmute(len) }
+        unsafe { mem::transmute(len) }
     }
 }
 
@@ -62,19 +62,41 @@ impl<'slice> Utf8CharIter<'slice> {
     ///
     /// # Safety
     /// There must be at least `n` bytes available in the backing iterator
-    unsafe fn fill_buf(&mut self, buf: &mut [u8; 4], n: Utf8CharEncodedLength) {}
+    unsafe fn fill_buf(&mut self, buf: &mut [u8; 4], n: Utf8CharEncodedLength) {
+        // SAFETY: caller has ensured backing iterator has enough bytes to fill the requested
+        // amount of 1..=4 and advance the iterator by the same amount
+        //
+        // this is a very idiomatic way to copy these bytes, its possibly not very efficient but it
+        // is trivially correct
+        unsafe {
+            ptr::copy_nonoverlapping(self.ptr.as_ptr(), buf.as_mut_ptr(), n as usize);
+            self.ptr = self.ptr.add(n as usize);
+        }
+    }
 
     /// Constructs a new `Utf8CharIter` from a string slice, borrowing the slice
-    fn new(s: &'slice str) -> Self {}
+    fn new(s: &'slice str) -> Self {
+        // SAFETY: String reference cannot be null
+        let ptr = unsafe { NonNull::new_unchecked(s.as_ptr().cast_mut()) };
+
+        // SAFETY: It is always sound to add the length of an allocation to its start pointer, see
+        // ptr::add docs for clarification
+        let end = unsafe { ptr.add(s.len()) };
+
+        // lexically express captured lifetime
+        let lifetime = PhantomData::<&'slice str>;
+
+        Self { ptr, end, lifetime }
+    }
 
     /// # Safety
     /// There must be at least one more codepoint in the backing utf8 slice
     unsafe fn next_unchecked(&mut self) -> Utf8Char {
         // SAFETY: caller ensures pseudo-slice is not empty; we have provenance over the byte behind ptr
-        let len = Utf8Char::codepoint_len(unsafe { self.ptr.read() });
+        let first = unsafe { self.ptr.read() };
 
-        // SAFETY: Utf8Char::codepoint_len will always return 1..=4 for a utf8 encoded character's first byte
-        unsafe { assert_unchecked((1..=4).contains(&len)) };
+        // SAFETY: first is the first byte of a potentially multibyte utf8 encoded character
+        let len = unsafe { Utf8CharFirstByte::new(first).codepoint_len() };
 
         // construct dummy Utf8Char for overwriting purposes
         let mut ch = const { Utf8Char::from_char('0') };
@@ -84,16 +106,9 @@ impl<'slice> Utf8CharIter<'slice> {
         // safety invariants
         let mut arr = *ch.0.as_array();
 
-        // SAFETY: len is derived from the utf8 len of the codepoint, meaning self.ptr has at least
-        // that many bytes available for reading, arr has len=4 which is within the 1..=4 range
-        // that len inhabits
-        //
-        // this is a very idiomatic way to copy these bytes, its possibly not very efficient but it
-        // is trivially correct
-        unsafe {
-            ptr::copy_nonoverlapping(self.ptr.as_ptr(), arr.as_mut_ptr(), usize::from(len));
-            self.ptr = self.ptr.add(usize::from(len));
-        }
+        // SAFETY: There are at least len bytes available in the backing iterator because the utf8
+        // codepoint's first byte indicated the length and we are iterating over valid utf8
+        unsafe { self.fill_buf(&mut arr, len) };
 
         // SAFETY: arr matches the invariants of utf8char because it was built from a null utf8char and
         // then had 1..=4 bytes of a single unicode codepoint copied, leaving padding intact
