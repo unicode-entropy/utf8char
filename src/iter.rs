4b414c54820c1fb0e6a56389119f5a25d8e5789a
ultrabear <bearodark@gmail.com>
2025-04-06 00:40:21 -0700

    just use slice::Iter its as fast or faster...

--- src/iter.rs
+++ src/iter.rs
@@ -1,71 +1,25 @@
 //! Implements an Iterator on strings that provide Utf8Char's
 
-use core::{
-    iter::FusedIterator,
-    marker::PhantomData,
-    ptr::{self, NonNull},
-    slice,
-};
+use core::{iter::FusedIterator, slice};
 
 use crate::{representation::Utf8FirstByte, Utf8Char};
 
 /// An iterator over a string that yields `Utf8Char`'s
 #[derive(Clone, Debug)]
 pub struct Utf8CharIter<'slice> {
+    /// benchmarked, just using this and its normal api is the fastest way already
+    /// dont bother writing some manual pointer magic to try and beat it... dont ask
     inner: slice::Iter<'slice, u8>,
 }
 
-/// Crafted so as to optimize into nothing, but give us raw copies of the pointers
-fn iter_to_raw<'a>(s: slice::Iter<'a, u8>) -> (NonNull<u8>, NonNull<u8>, PhantomData<&'a str>) {
-    let r = s.as_slice().as_ptr_range();
-
-    (
-        // SAFETY: slice::Iter pointers cannot be null on a non ZST
-        unsafe { NonNull::new_unchecked(r.start.cast_mut()) },
-        // SAFETY: slice::Iter pointers cannot be null on a non ZST
-        unsafe { NonNull::new_unchecked(r.end.cast_mut()) },
-        PhantomData,
-    )
-}
-
-/// SAFETY: start, end, and lt must be derived from iter_to_raw
-unsafe fn raw_to_iter<'a>(
-    start: NonNull<u8>,
-    end: NonNull<u8>,
-    lt: PhantomData<&'a str>,
-) -> slice::Iter<'a, u8> {
-    let _consumed_in_typesignature = lt;
-
-    // SAFETY: caller promises start and end were derived from iter_to_raw which is in turn backed
-    // by a valid slice
-    unsafe { slice::from_raw_parts(start.as_ptr(), end.offset_from(start) as usize) }.iter()
-}
-
 impl<'slice> Utf8CharIter<'slice> {
-    /// Advances the backing iterator by n bytes
+    /// Returns the next byte and advances the backing iterator
     ///
     /// # Safety
-    /// There must be at least n bytes available in the backing iterator to be advanced over
-    unsafe fn advance_unchecked(&mut self, n: usize) {
-        let (mut start, end, lt) = iter_to_raw(self.inner.clone());
-
-        // SAFETY: caller has asserted there are at least n bytes available in backing array
-        start = unsafe { start.add(n) };
-
-        // SAFETY: start/end/lt were constructed from iter_to_raw, and start was modified to a
-        // still valid state
-        self.inner = unsafe { raw_to_iter(start, end, lt) };
-    }
-
-    /// Peeks the n'th next byte without advancing the iterator
-    ///
-    /// # Safety
-    /// The backing iterator must have at least N+1 bytes available for reading
-    unsafe fn peek_unchecked(&self, n: usize) -> u8 {
-        let (read, _, _) = iter_to_raw(self.inner.clone());
-
-        // SAFETY: Caller has asserted there are at least n+1 more bytes left to be read
-        unsafe { read.add(n).read() }
+    /// There must be at least one byte left in the backing iterator
+    unsafe fn next_byte_unchecked(&mut self) -> u8 {
+        // SAFETY: Caller asserts there is at least one byte left in the backing iterator
+        *unsafe { self.inner.next().unwrap_unchecked() }
     }
 
     /// Constructs a new `Utf8CharIter` from a string slice, borrowing the slice
@@ -79,7 +33,7 @@ impl<'slice> Utf8CharIter<'slice> {
     /// There must be at least one more codepoint in the backing utf8 slice
     unsafe fn next_unchecked(&mut self) -> Utf8Char {
         // SAFETY: caller ensures pseudo-slice is not empty; we have a byte available to read
-        let first = unsafe { self.peek_unchecked(0) };
+        let first = unsafe { self.next_byte_unchecked() };
 
         // SAFETY: first is the first byte of a potentially multibyte utf8 encoded character
         let len = unsafe { Utf8FirstByte::new(first).codepoint_len() } as u8;
@@ -95,24 +49,20 @@ impl<'slice> Utf8CharIter<'slice> {
         // 0th byte is always used in utf8
         arr[0] = first;
 
-        // for each len>n peek_unchecked(n) is valid
         if len > 1 {
             // SAFETY: len>1 so the 1st byte is valid
-            arr[1] = unsafe { self.peek_unchecked(1) };
+            arr[1] = unsafe { self.next_byte_unchecked() };
         }
 
         if len > 2 {
             // SAFETY: len>2 so the 2nd byte is valid
-            arr[2] = unsafe { self.peek_unchecked(2) };
+            arr[2] = unsafe { self.next_byte_unchecked() };
         }
 
         if len > 3 {
             // SAFETY: len>3 so the 3rd byte is valid
-            arr[3] = unsafe { self.peek_unchecked(3) };
+            arr[3] = unsafe { self.next_byte_unchecked() };
         }
-
-        // SAFETY: advance by the length we just read, which must be valid
-        unsafe { self.advance_unchecked(len as usize) };
 
         // SAFETY: arr matches the invariants of utf8char because it was built from a null utf8char and
         // then had 1..=4 bytes of a single unicode codepoint copied, leaving padding intact
