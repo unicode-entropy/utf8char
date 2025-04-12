b83943f3602c2391cb38a95ced02af29a7c1a77a
ultrabear <bearodark@gmail.com>
2025-04-12 16:25:06 -0700

    carve out invalid utf8 firstbyte values

--- src/representation/utf8firstbyte.py
+++ src/representation/utf8firstbyte.py
@@ -1,8 +1,30 @@
+from typing import Callable
+
+filters: list[tuple[Callable[[int], bool], str]] = [
+    (lambda i: i >= 0b1111_1000, "everything with 5 contiguous 1 bits is invalid"),
+    (lambda i: (i & 0b1100_0000) == 0b1000_0000, "unicode cont bytes are invalid"),
+    (
+        lambda i: i in {0b1100_0000, 0b1100_0001},
+        "guaranteed overlong encodings are invalid",
+    ),
+]
+
+
 def main():
     for i in range(128, 256):
-        # everything with 5 contiguous 1 bits is invalid
-        if not i >= 0b1111_1000:
-            print(f"    _{i} = 0b{i:b},")
+        valid = True
+
+        for f in filters:
+            matches, err = f
+
+            if matches(i):
+                print(f"    // SKIP: _{i} = 0b{i:b}, {err}")
+                valid = False
+                break
+
+        if valid:
+            print(f"    _{i} = 0b{i:b}, // {i:x}")
+
 
 if __name__ == "__main__":
     main()
